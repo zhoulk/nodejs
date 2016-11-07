c77c11f076fd1205f1fa3c66d412867529a938f2
zhoulk <849693228@qq.com>
2016-11-08 02:25:46 +0800

    MVC初步

--- koa/app.js
+++ koa/app.js
@@ -1,41 +1,42 @@
 const Koa = require('koa');
 
+const bodyParser = require('koa-bodyparser');
+
 const controller = require('./controller');
 
 const templating = require('./templating');
 
-// const router = require('koa-router')();
-
-var app = new Koa();
+const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(async(ctx, next) => {
+// log request URL:
+app.use(async (ctx, next) => {
 	console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+	var
+		start = new Date().getTime(),
+		execTime;
 	await next();
-    // ctx.response.type = 'text/html';
-    // ctx.response.body = '<h1>Hello, koa2!</h1>';
+	execTime = new Date().getTime() - start;
+	ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
-//
-// // add url-route:
-// router.get('/hello/:name', async (ctx, next) => {
-//     var name = ctx.params.name;
-//     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-// });
-//
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = '<h1>Index</h1>';
-// });
-//
-// // add router middleware:
-// app.use(router.routes());
 
+// static file support:
+if (! isProduction) {
+	let staticFiles = require('./static-files');
+	app.use(staticFiles('/static/', __dirname + '/static'));
+}
 
+// parse request body:
+app.use(bodyParser());
+
+// add nunjucks as view:
 app.use(templating('views', {
 	noCache: !isProduction,
 	watch: !isProduction
 }));
 
+// add controller:
 app.use(controller());
 
 app.listen(3000);
